d3a7d75825661d4ceda1915219ba99b33c03eeac
paste-fiuba <mpasteloff@fi.uba.ar>
2026-04-05 03:14:52 -0300

    fix puerto dinamico

--- backend/src/server.ts
+++ backend/src/server.ts
@@ -1,11 +1,11 @@
-import app from './app.js'; 
+import app from './app.js';
 import dotenv from 'dotenv';
 
 dotenv.config();
 
 const PORT = process.env.PORT || 5000;
 
-// Arrancamos el servidor
-app.listen(PORT, () => {
-    console.log(`Servidor corriendo en el puerto ${PORT}`);
+// 2. Escuchamos en el puerto DINÁMICO y en todas las interfaces ('0.0.0.0')
+app.listen(Number(PORT), '0.0.0.0', () => {
+    console.log(`🚀 Servidor listo y escuchando en el puerto ${PORT}`);
 });
